cff5584bc3eb9905d163fdf80269685bde7ddedd
VictorVas98 <abel02172@gmail.com>
2023-06-10 10:21:37 -0600

    Initial commit

---
+++ ExamenFinal/ExamenFinal/enlace.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenFinal
+{
+    internal class enlace
+    {
+        public const string CONECCION = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Victo\\OneDrive\\Escritorio\\PROYECTO\\ExamenFinal\\ExamenFinal\\Alumno.mdf;Integrated Security=True";
+    }
+}
